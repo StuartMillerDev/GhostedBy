801c5b11e4630ca92ab006fa5fc5cddd3047b52a
LeRon Jackson <leronj23@gmail.com>
2019-04-02 18:27:17 -0400

    update

--- public/js/index.js
+++ public/js/index.js
@@ -177,6 +177,13 @@ function getCompanyReportedName() {
 
   var place = autocompleteReport.getPlace();
 
+  if (!place.geometry) {
+    // User entered the name of a Place that was not suggested and
+    // pressed the Enter key, or the Place Details request failed.
+    window.alert("No details available for input: '" + place.name + "'");
+    return;
+  }
+
   document.getElementById("report-company").value = '';
   document.getElementById("report-company").value = place.name;
 
@@ -186,6 +193,13 @@ function getCompanyReportedName() {
 function getCompanyLookupName() {
 
   var place = autocompleteLookup.getPlace();
+
+  if (!place.geometry) {
+    // User entered the name of a Place that was not suggested and
+    // pressed the Enter key, or the Place Details request failed.
+    window.alert("No details available for input: '" + place.name + "'");
+    return;
+  }
 
   document.getElementById("lookup-company").value = '';
   document.getElementById("lookup-company").value = place.name;
